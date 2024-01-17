2bee83b27bbdd80fcf4f1bd30ab915426ab89a3c
oriabukha <alerya@hotmail.com>
2024-01-17 20:27:37 +0100

    greet route

--- real_world_clojure_api/core.clj
+++ real_world_clojure_api/core.clj
@@ -1,6 +1,25 @@
 (ns real-world-clojure-api.core
-  (:require [real-world-clojure-api.config :as config]))
+  (:require [real-world-clojure-api.config :as config]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(defn respond-hello [request]
+  {:status 200 :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server [config]
+  (http/create-server
+   {::http/routes routes
+    ::http/type :jetty
+    ::http/port (-> config :server :port)}))
+
+(defn start [config]
+  (http/start (create-server config)))
 
 (defn -main []
   (let [config (config/read-config)]
-    (println "Starting API Service... " config)))
+    (println "Starting API Service... " config)
+    (start config)))
